90ea9fa8e319f9de24d3e4473e910da4ad5f7309
yourenz <yourenz_work@qq.com>
2024-01-05 09:14:36 +0800

    chore(routers):  auth config

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,17 +1,33 @@
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, redirect } from 'react-router-dom'
+import { getDefaultStore } from 'jotai'
 import Layout from '@/layout'
 import Login from '@/pages/Login'
 import RouteErrorBoundary from '@/components/RouteErrorBoundary'
+import { loginAtom } from '@/atoms/system'
+
+const defaultStore = getDefaultStore()
+const isLogin = defaultStore.get(loginAtom)
 
 const Routers = createBrowserRouter([
   {
     path: '/',
     errorElement: <RouteErrorBoundary />,
     element: <Layout />,
+    loader: async () => {
+      if (!isLogin)
+        throw redirect('/login')
+      return {}
+    },
+
   },
   {
     path: '/login',
     element: <Login />,
+    loader: async () => {
+      if (isLogin)
+        throw redirect('/')
+      return {}
+    },
   },
 ])
 
