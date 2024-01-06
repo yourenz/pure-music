718895fa43ff6e10acb81963ed45a2a98b324c5f
yourenz <yourenz_work@qq.com>
2024-01-06 18:34:31 +0800

    chore(tauri): http server support

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,19 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod server;
+use std::thread;
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .setup(|app| {
+            let handle = app.handle();
+            let boxed_handle = Box::new(handle);
+            thread::spawn(move || {
+                server::init(*boxed_handle).unwrap();
+            });
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
---
+++ src-tauri/src/server/mod.rs
@@ -0,0 +1,25 @@
+use std::sync::Mutex;
+use actix_files::Files;
+use actix_web::{middleware, web, App, HttpServer};
+use tauri::AppHandle;
+
+struct TauriAppState {
+    #[allow(dead_code)]
+    app: Mutex<AppHandle>,
+}
+
+#[actix_web::main]
+pub async fn init(app: AppHandle) -> std::io::Result<()> {
+    let tauri_app = web::Data::new(TauriAppState {
+        app: Mutex::new(app),
+    });
+    HttpServer::new(move || {
+        App::new()
+            .app_data(tauri_app.clone())
+            .wrap(middleware::Logger::default())
+            .service(Files::new("/callback", "./public/").index_file("callback.html"))
+    })
+    .bind(("127.0.0.1", 58944))?
+    .run()
+    .await
+}
