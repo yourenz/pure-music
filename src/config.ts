a450b9da6099bae35a0133375bd0f7051c4c351b
yourenz <yourenz_work@qq.com>
2024-01-07 21:15:04 +0800

    chore(config): add redirectUri

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,26 @@
+const isTauri = !!window?.__TAURI__
+const isDev = import.meta.env.MODE === 'development'
+
+function getRedirectUri() {
+  let redirectUri = ''
+  if (isDev) {
+    if (isTauri)
+      redirectUri = 'http://127.0.0.1:58944/callback'
+    else
+      redirectUri = 'http://127.0.0.1:5173/callback'
+  }
+  else {
+    if (isTauri)
+      redirectUri = 'http://127.0.0.1:58944/callback'
+    else
+    // prod redirectUri
+      redirectUri = ''
+  }
+  return redirectUri
+}
+
 export default {
-  isTauri: !!window?.__TAURI__,
-  isDev: import.meta.env.MODE === 'development',
+  isTauri,
+  isDev,
+  redirectUri: getRedirectUri(),
 }
