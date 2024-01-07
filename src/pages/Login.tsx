4bc2dac8ef9ea370908086c7cd56466162f3994d
yourenz <yourenz_work@qq.com>
2024-01-07 21:42:59 +0800

    feat:  login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,55 @@
+import { useTranslation } from 'react-i18next'
+
+import { Icons } from '@/components/ui/Icon'
+
+import { Button } from '@/components/ui/Button'
+import SpotifyWeb from '@/SpotifyWeb'
+import config from '@/config'
+
 const Login: React.FC = () => {
+  const { t } = useTranslation()
+
+  const handleLogin = () => {
+    const S = new SpotifyWeb({
+      clientId: import.meta.env.VITE_SPOTIFY_CLIENT_ID,
+      redirectUri: config.redirectUri,
+    })
+    S.redirectToAuthCodeFlow()
+  }
+
   return (
-    <span>login</span>
+    <>
+      <div className="container relative hidden h-full flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+
+        <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
+          <div className="absolute inset-0 bg-zinc-900" />
+          <div className="relative z-20 flex items-center text-lg font-medium">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="mr-2 h-6 w-6"
+            >
+              <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
+            </svg>
+            Pure Music
+          </div>
+
+        </div>
+        <div className="lg:p-8">
+          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+            <Button variant="outline" type="button" onClick={handleLogin}>
+              <Icons.icon className="mr-2 h-6 w-6" />
+              {t(`pages.login.loginBtnText`)}
+            </Button>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
