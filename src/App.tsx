8cb1b7fdb2fee932c3f927db2205f83df2aa2222
yourenz <yourenz_work@qq.com>
2024-01-07 21:35:35 +0800

    feat:  deep-link listen

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,16 @@ import '@/i18n/config.ts'
 import './css/init.css'
 import './css/global.css'
 import { RouterProvider } from 'react-router-dom'
+import useTauriListen from '@/hooks/useTauriListen'
 import router from '@/routers'
 import { Toaster } from '@/components/ui/Sonner'
 
 const App: React.FC = () => {
+  useTauriListen({ eventName: 'pure-music-received', cb: (paylod) => {
+    const code = paylod?.substring(paylod?.indexOf('=') + 1)
+    window.location.href = `/callback?${code}`
+  } })
+
   return (
     <>
       <Toaster />
