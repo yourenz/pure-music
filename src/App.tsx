3c91fcc4c292d33787748db674686a70041fe5c2
yourenz <yourenz_work@qq.com>
2024-01-09 23:09:09 +0800

    chore(request): add react-query

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,19 @@ import '@/i18n/config.ts'
 import './css/init.css'
 import './css/global.css'
 import { RouterProvider } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import useTauriListen from '@/hooks/useTauriListen'
 import router from '@/routers'
 import { Toaster } from '@/components/ui/Sonner'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+    },
+  },
+})
 
 const App: React.FC = () => {
   useTauriListen({ eventName: 'pure-music-received', cb: (paylod) => {
@@ -15,7 +25,9 @@ const App: React.FC = () => {
   return (
     <>
       <Toaster />
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </>
   )
 }
